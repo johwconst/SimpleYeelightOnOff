dca47ccfad8cde880b0d47b837e2eac70ef3f898
JohnatanDuarte <joh-duarte@hotmail.com>
2020-06-19 02:15:05 -0300

    increase interface and names

--- test.py
+++ test.py
@@ -11,10 +11,7 @@ class mainWindow(tk.Frame):
 
     def __init__(self, master=None):
         super().__init__(master)
-        largura = round(self.winfo_screenwidth() / 2)
-        altura = round(self.winfo_screenheight() / 2)
-        altura = round(self.winfo_screenheight() / 2)
-        tamanho = ('%sx%s' % (largura, altura))
+        tamanho = ('%sx%s' % (770, 370))
 
         master.title('Simple Yeelight 2 Manage')
         master.geometry(tamanho)
@@ -29,7 +26,7 @@ class mainWindow(tk.Frame):
     def createWid(self):
         # Create frames
         frame1 = tk.Frame(self)
-        frame1.pack(side=tk.TOP, fill=tk.BOTH, padx=5, pady=5)
+        frame1.pack(side=tk.TOP, fill=tk.BOTH, padx=120, pady=10, anchor='center')
 
         frame2 = tk.Frame(self)
         frame2.pack(fill=tk.BOTH, expand=True)
@@ -38,13 +35,13 @@ class mainWindow(tk.Frame):
         frame3.pack(side=tk.BOTTOM, padx=5)
 
         # Labels
-        name_label = tk.Label(frame1, text='Nome')
+        name_label = tk.Label(frame1, text='Name')
         name_label.grid(row=0, column=0)
 
         ip_label = tk.Label(frame1, text='IP')
         ip_label.grid(row=0, column=1)
 
-        location_label = tk.Label(frame1, text='Localização')
+        location_label = tk.Label(frame1, text='Location')
         location_label.grid(row=0, column=2)
 
         # Entry Text
@@ -59,15 +56,16 @@ class mainWindow(tk.Frame):
 
         # Add Button
         button_add = tk.Button(frame1, text='Add Lamp', bg='blue', fg='white')
+        button_add['font'] = ("Arial", "12", "bold")
         button_add['command'] = self.addLamp
         button_add.grid(row=0, column=3, rowspan=2, padx=10)
 
         # Treeview.
         self.treeview = tkk.Treeview(frame2, columns=('Nome', 'IP', 'Localização'))
         self.treeview.heading('#0', text='ID')
-        self.treeview.heading('#1', text='Nome')
+        self.treeview.heading('#1', text='Name')
         self.treeview.heading('#2', text='IP')
-        self.treeview.heading('#3', text='Localização')
+        self.treeview.heading('#3', text='Location')
 
         for row in self.db.getLamp():
             self.treeview.insert('', 'end', text=row[0], values=(row[1], row[2], row[3]))
@@ -76,18 +74,21 @@ class mainWindow(tk.Frame):
 
         # Delete Button
         button_delete = tk.Button(frame3, text='Delete', bg='red', fg='white')
+        button_delete['font'] = ("Arial", "12", "bold")
         button_delete['command'] = self.deleteLamp
-        button_delete.pack(pady=8)
+        button_delete.pack(side='left', padx=10)
 
         # Turn On Button
         button_on = tk.Button(frame3, text="Turn On", bg="yellow", fg='black')
+        button_on['font'] = ("Arial", "12", "bold")
         button_on['command'] = self.turnOn
-        button_on.pack(pady=10)
+        button_on.pack(side='left', padx=10)
 
         # Turn Off Button
         button_off = tk.Button(frame3, text="Turn Off", bg="black", fg='white')
+        button_off['font'] = ("Arial", "12", "bold")
         button_off['command'] = self.turnOff
-        button_off.pack(pady=10)
+        button_off.pack(side='left', padx=10)
 
     def turnOn(self):
         if not self.treeview.focus():
